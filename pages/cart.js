20fa2a9d653526cfcb551a6cd21734422b4666e6
Emanuel <emanuel.scalco@gmail.com>
2021-10-05 17:39:50 -0300

    Scren Cart Fix

--- pages/cart.js
+++ pages/cart.js
@@ -1,4 +1,5 @@
 import NextLink from 'next/link';
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import {
   Button,
@@ -21,7 +22,7 @@ import React, { useContext } from 'react';
 import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 
-export default function CartScreen() {
+function CartScreen() {
   const { state } = useContext(Store);
   const {
     cart: { cartItems },
@@ -116,3 +117,5 @@ export default function CartScreen() {
     </Layout>
   );
 }
+
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
